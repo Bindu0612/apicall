e33b86c928b9cc3e08b611e7a7e2ca7ea597e85d
Bindu0612 <58510359+Bindu0612@users.noreply.github.com>
2020-05-07 15:21:29 +0530

    code clean up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
 import NewAp from './NewAp';
-// import Myapi from './Myapi';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <NewAp /> 
-          </BrowserRouter>,
+    <NewAp />
+  </BrowserRouter>,
   document.getElementById('root')
 );
